43ee57c30a03af15d094bee2badecb68a5c2659b
MrMeowCat <viktorzablotsky@gmail.com>
2017-11-01 23:07:19 +0300

    add disease dto, test disease repo

---
+++ src/main/java/com/instinctools/padlaboris/dto/DiseaseDto.java
@@ -0,0 +1,21 @@
+package com.instinctools.padlaboris.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DiseaseDto implements Serializable {
+
+    private static final long serialVersionUID = 2710050838305766544L;
+
+    private int id;
+    private String diseaseName;
+    private String diseaseCode;
+    private String diseaseDescription;
+    private String diseaseClass;
+}
---
+++ src/test/java/com/instinctools/padlaboris/service/DiseaseCrudTest.java
@@ -0,0 +1,66 @@
+package com.instinctools.padlaboris.service;
+
+import com.instinctools.padlaboris.model.Disease;
+import com.instinctools.padlaboris.repository.DiseaseRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class DiseaseCrudTest {
+
+    @Autowired
+    DiseaseRepository diseaseRepository;
+
+    @Before
+    public void init() {
+        Disease disease = new Disease(
+                null,
+                "cancer",
+                "123",
+                "you're dead",
+                "hardcore");
+
+        diseaseRepository.save(disease);
+    }
+
+    @Test
+    public void testFind() {
+        List list = diseaseRepository.findAll();
+        assertEquals(1, list.size());
+    }
+
+    @Test
+    public void testFindByParameter() {
+        List list = diseaseRepository.findByDiseaseName("cancer");
+        assertEquals(1, list.size());
+
+        list = diseaseRepository.findByDiseaseClass("hard");
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void testUpdate() {
+        Disease disease = diseaseRepository.findOne(1);
+        disease.setDiseaseName("ASJDNAIEUFOEF");
+        diseaseRepository.save(disease);
+
+        disease = diseaseRepository.findOne(1);
+        assertNotEquals("cancer", disease.getDiseaseName());
+    }
+
+    @Test
+    public void delete() {
+        diseaseRepository.delete(1);
+        assertEquals(0, diseaseRepository.findAll().size());
+    }
+}
